5c6dfc1b55b5a892d7dcdfb1555f72e4214ddab5
Aleksey Seredov <Alexey.Seredov@bercut.com>
2023-05-19 13:58:28 +0300

    home work lesson2 with gtest

--- lib.h
+++ lib.h
@@ -1,3 +1,41 @@
 #pragma once
 
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#define UNUSED(variable) (void)variable
+
+using VectorString = std::vector<std::string>;
+using VecStrIter = VectorString::const_iterator;
+using VectorVectorString = std::vector<VectorString>;
+using VecVecStringIter = std::vector<VectorString>::const_iterator;
 int version();
+
+std::vector<std::string> split(const std::string &str, char d);
+
+template <typename F>
+void printOn(VectorVectorString &ip_pool, F &&func) {
+    auto result = std::find_if(ip_pool.begin(), ip_pool.end(), func);
+
+    while (result != ip_pool.end()) {
+        for (auto ip_part = result->cbegin(); ip_part != result->cend(); ++ip_part) {
+            if (ip_part != result->cbegin()) {
+                std::cout << ".";
+            }
+            std::cout << *ip_part;
+        }
+        std::cout << std::endl;
+        result = std::find_if(++result, ip_pool.end(), func);
+    };
+}
+
+void printOn(VectorVectorString &ip_pool);
+
+void sortIP(VectorVectorString &ip_pool, bool direction = true);
+
+void filter(VectorVectorString &ip_pool, int first);
+void filter(VectorVectorString &ip_pool, int first, int second);
+void filter_any(VectorVectorString &ip_pool, int byte);
--- test_version.cpp
+++ test_version.cpp
@@ -1,16 +1,74 @@
-#include <iostream>
-#include "gtest/gtest.h"
 #include "lib.h"
+#include "gtest/gtest.h"
+#include <iostream>
 
+class IPTest : public ::testing::Test {
+  protected:
+    void SetUp() override {
+        // q0_ remains empty
+        ip_pool.push_back(split("113.162.145.156", '.'));
+        ip_pool.push_back(split("157.39.22.224", '.'));
+        ip_pool.push_back(split("79.180.73.190", '.'));
+        ip_pool.push_back(split("1.29.168.152", '.'));
+        ip_pool.push_back(split("46.182.19.219", '.'));
+        ip_pool.push_back(split("154.160.1.180", '.'));
+        ip_pool.push_back(split("168.195.231.1", '.'));
+        ip_pool.push_back(split("46.70.113.73", '.'));
+        ip_pool.push_back(split("185.84.46.70", '.'));
+        ip_pool.push_back(split("89.46.70.220", '.'));
+        ip_pool.push_back(split("185.84.47.70", '.'));
+        ip_pool.push_back(split("46.70.225.39", '.'));
+    }
 
+    // void TearDown() override {}
+  public:
+    VectorVectorString ip_pool;
+};
 
-TEST(test_version, test_valid_version)
-{
+TEST_F(IPTest, test_sort_desc) {
+    sortIP(ip_pool, false);
+    VectorString v = ip_pool.at(0);
+    EXPECT_STREQ(ip_pool.begin()->begin()->c_str(), "185");
+    EXPECT_STREQ(ip_pool.begin()->at(2).c_str(), "47");
+    EXPECT_TRUE(atoi(ip_pool.rbegin()->begin()->c_str()) == 1);
+}
+
+TEST_F(IPTest, test_value_1) {
+    testing::internal::CaptureStdout();
+    auto finder1 = [value = 1](auto a) { return value == atoi(a.cbegin()->c_str()); };
+    printOn(ip_pool, finder1);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_STREQ(output.c_str(), "1.29.168.152\n");
+}
+
+TEST_F(IPTest, test_value_46_70) {
+    testing::internal::CaptureStdout();
+    auto finder2 = [value1 = 46, value2 = 70](VectorString a) { return value1 == atoi(a.at(0).c_str()) && value2 == atoi(a.at(1).c_str()); };
+    printOn(ip_pool, finder2);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_STREQ(output.c_str(), "46.70.113.73\n46.70.225.39\n");
+}
+
+TEST_F(IPTest, test_value_46) {
+    testing::internal::CaptureStdout();
+    filter_any(ip_pool, 46);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_STREQ(output.c_str(), "46.182.19.219\n46.70.113.73\n185.84.46.70\n89.46.70.220\n46.70.225.39\n");
+}
+
+TEST_F(IPTest, test_value_46_sort_desc) {
+    testing::internal::CaptureStdout();
+    sortIP(ip_pool, false);
+    filter_any(ip_pool, 46);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_STREQ(output.c_str(), "185.84.46.70\n89.46.70.220\n46.182.19.219\n46.70.225.39\n46.70.113.73\n");
+}
+
+TEST(test_version, test_valid_version) {
     EXPECT_TRUE(version() > 0);
 }
 
-int main (int argc, char ** argv) {
+int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-
